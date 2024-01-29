f52e9f7b471a5396ae117c24d8a4d940df316d36
nathan <racecn@gmail.com>
2024-01-28 20:30:33 -0500

    Tailwind Init Check

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
