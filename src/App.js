b6fb50d45edf041522432c667785f8b739dff2f8
hello202320 <druidletsplay2022@gmail.com>
2024-02-05 13:47:33 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import  {BrowserRouter, Routes, Route} from 'react-router-dom';
+import navbar from './components/navbar';
 import Home from './pages/Home'
+import Login from './pages/Login';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar/>
         <Routes>
           <Route index element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom"
+
+export default function(){
+    return(
+    <>  
+        <Link to="/"> Home</Link>
+        <Link to="/login">Login</Link>
+    </>
+ 
+    )
+}
