8a5c49644514c201f5b7891ee233ec641209d64b
hello202320 <druidletsplay2022@gmail.com>
2024-02-05 14:37:14 -0500

    added auth state for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,21 @@
 import {Link} from "react-router-dom"
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth} from '../firebase.js'
 export default function(){
+    const [user] = useAuthState(auth);
     return(
-    <>  
+       
+    <navbar>
         <Link to="/"> Home</Link>
-        <Link to="/login">Login</Link>
-    </>
+        {!user ? <Link to="/login">Login</Link>: 
+            <>
+                Posts
+            </>
+        }
+       
+    </navbar>
+        
+    
  
     )
 }
