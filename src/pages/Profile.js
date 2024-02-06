1289903c6c5f4da135cceaaed55940886f7b2c60
hello202320 <druidletsplay2022@gmail.com>
2024-02-06 16:43:08 -0500

    fixed profile link

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import {auth} from '../firebase.js'
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from 'react-firebase-hooks/auth';
 
-export default function Login(){
+export default function Profile(){
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
      
