eeadccc8975398e2ac2681bba19a0ef4490e6558
hello202320 <druidletsplay2022@gmail.com>
2024-02-06 17:16:43 -0500

    prepare merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import  {BrowserRouter, Routes, Route} from 'react-router-dom';
-import navbar from './components/navbar';
 import Home from './pages/Home'
 import Login from './pages/Login';
 import Navbar from './components/navbar';
