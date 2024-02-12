d4fcb1480eb16ef4c4650a8458569769bd5006a9
Nathan Race <racecn@gmail.com>
2024-02-11 23:07:55 -0500

    Moved login modal activation to nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import {Link} from "react-router-dom"
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { signOut } from "@firebase/auth";
 import { auth } from "../firebase";
+import Modal from "./Modal";
 
 const logout = () => {
     signOut(auth);
@@ -13,7 +14,8 @@ export default function(){
        
     <nav className="flex flex-row gap-10 p-4">
         <Link to="/"> Home</Link>
-        {!user ? <Link to="/login">Login</Link>: 
+        {!user ? <Modal/>: 
+
             <>
                 <Link to="/profile">Profile</Link>
                 <button className='logout-button' onClick={logout}>Sign off</button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,7 +35,7 @@ export default function Home() {
     return (
         <main>
             {!user ? (
-                <Modal />
+                <div> logged out</div>
             ) : (
                 <>
                     {user.displayName && (
