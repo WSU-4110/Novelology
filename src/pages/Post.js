50f7785d8fa85aaacdbc85f5e72290bd9daa563c
hello202320 <druidletsplay2022@gmail.com>
2024-02-20 15:04:06 -0500

    prepare merge

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,22 +1,31 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import {auth} from '../firebase.js'
+
+import {auth,db} from '../firebase.js'
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { getFirestore, doc } from 'firebase/firestore';
+import { useDocument } from 'react-firebase-hooks/firestore';
 
 export default function Post(){
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
+    const docRef = doc(db, 'posts', 'hhacGRM6jCc3qaF6pcgJ')
+    const [value, loading, error] = useDocument(docRef)
 
-    const login = async() => {
-        const provider = new GoogleAuthProvider();
-        const result = await signInWithPopup(auth, provider);
-        navigate('/');
-      };
+   
+  
+
      
     return (
         
             <div>
-              post
+                    <p>
+                            {error && <strong>Error: {JSON.stringify(error)}</strong>}
+                            {loading && <span>Document: Loading...</span>}
+                            {console.log(JSON.stringify(value.data()))}
+                            
+                            {value && <span>Document: {JSON.stringify(value.data())}</span>}
+                    </p>
+
             </div>
         
     )
