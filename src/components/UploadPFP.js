7086c5516ae656876d58f0afac71faa9d72bb51c
Nathan Race <racecn@gmail.com>
2024-02-19 19:48:11 -0500

    Cropping init

--- src/components/UploadPFP.js
+++ src/components/UploadPFP.js
@@ -1,119 +1,141 @@
 import React, { useState, useEffect } from 'react';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 import { auth } from '../firebase';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
 import defaultProfilePicture from '../assets/default-profile-picture.jpg'; // Import default profile picture
 import fetchPFP from '../functions/fetchPFP'; // Import fetchPFP function
 
 const UploadPFP = () => {
+  // State variables
   const [image, setImage] = useState(null);
+  const [crop, setCrop] = useState({ unit: '%', width: 50, aspect: 1 / 1 });
+  const [croppedImage, setCroppedImage] = useState(null);
   const [error, setError] = useState(null);
   const [existingProfilePicture, setExistingProfilePicture] = useState(null);
-
-  useEffect(() => {
-    const fetchExistingProfilePicture = async () => {
-      try {
-        const userId = auth.currentUser.uid;
-        console.log('Fetching existing profile picture...');
-        const existingURL = await fetchPFP(userId);
-        if (existingURL) {
-          console.log('Existing profile picture found:', existingURL);
-          setExistingProfilePicture(existingURL);
-        } else {
-          console.log('No existing profile picture found.');
-          // If no existing profile picture is found, set defaultProfilePicture
-          setExistingProfilePicture(defaultProfilePicture);
-        }
-      } catch (error) {
-        console.error('Error fetching existing profile picture:', error);
-        // If there's an error fetching the profile picture, set defaultProfilePicture
-        setExistingProfilePicture(defaultProfilePicture);
-      }
-    };
-
-    fetchExistingProfilePicture();
-  }, []);
-
   const MAX_FILE_SIZE_MB = 1; // Maximum file size allowed in megabytes
 
+
+  // Function to handle image load
+  const onImageLoaded = (img) => {
+    setImage(img); // Update the image state with the loaded image
+  };
+
+// Function to handle crop completion
+const onCropComplete = (crop) => {
+  getCroppedImage(); // Call getCroppedImage when cropping is completed
+};
+
+  // Function to handle crop change
+  const onCropChange = (newCrop) => {
+    setCrop(newCrop); // Update the crop state
+  };
+
+// Function to get cropped image
+const getCroppedImage = () => {
+  if (image && crop.width && crop.height) {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width * scaleX;
+    canvas.height = crop.height * scaleY;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width * scaleX,
+      crop.height * scaleY
+    );
+
+    canvas.toBlob((blob) => {
+      setCroppedImage(blob); // Update the cropped image state
+    }, 'image/jpeg');
+  }
+};
+
+
+
+  
+  
+
+  // Function to handle file change
   const handleChange = (e) => {
     const selectedFile = e.target.files[0];
     if (!selectedFile) return;
-
+  
     // Check if file size exceeds the maximum allowed size
     if (selectedFile.size > MAX_FILE_SIZE_MB * 1024 * 1024) {
       setError(`File size exceeds the maximum allowed size of ${MAX_FILE_SIZE_MB} MB.`);
       setImage(null); // Reset image state
     } else {
       setError(null); // Clear any previous error message
-      setImage(selectedFile);
+      setImage(selectedFile); // Update image state with selected file
     }
   };
+  
 
-  const handleUpload = (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
-
-    if (!image) {
-      console.log('No image selected for upload.');
+  // Function to handle upload
+  const handleUpload = async () => {
+    if (!croppedImage) {
+      setError('Please select and crop an image before uploading.');
       return;
     }
 
-    const userId = auth.currentUser.uid;
-    const storage = getStorage();
-    const metadata = {
-      contentType: 'image/jpeg' // Set content type to JPEG
-    };
+    try {
+      const userId = auth.currentUser.uid;
+      const storage = getStorage();
+      const metadata = {
+        contentType: 'image/jpeg'
+      };
 
-    // Convert image to JPEG format
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    const img = new Image();
-    img.onload = async () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
-      canvas.toBlob(async (blob) => {
-        const storageRef = ref(storage, `users/${userId}/profilePicture.jpg`); // Save as .jpg
-        const uploadTask = uploadBytesResumable(storageRef, blob, metadata);
+      const storageRef = ref(storage, `users/${userId}/profilePicture.jpg`);
+      const uploadTask = uploadBytesResumable(storageRef, croppedImage, metadata);
 
-        console.log('Starting upload...');
-        uploadTask.on('state_changed',
-          (snapshot) => {
-            // Track upload progress if needed
-          },
-          (error) => {
-            console.error('Error uploading file:', error);
-            setError('Failed to upload profile picture. Please try again later.');
-          },
-          () => {
-            // Upload completed successfully, get the download URL
-            getDownloadURL(uploadTask.snapshot.ref)
-              .then((downloadURL) => {
-                console.log('File uploaded successfully:', downloadURL);
-                setExistingProfilePicture(downloadURL); // Update existing profile picture URL
-                localStorage.setItem('profilePicture', downloadURL); // Update profile picture URL in local storage
-                setImage(null); // Reset image state after upload
-              })
-              .catch((error) => {
-                console.error('Error getting download URL:', error);
-                setError('Failed to get download URL. Please try again later.');
-              });
-          }
-        );
-      }, 'image/jpeg');
-    };
-    img.src = URL.createObjectURL(image);
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          // Handle upload progress
+        },
+        (error) => {
+          console.error('Error uploading file:', error);
+          setError('Failed to upload profile picture. Please try again later.');
+        },
+        () => {
+          // Upload completed successfully, get the download URL
+          getDownloadURL(uploadTask.snapshot.ref)
+            .then((downloadURL) => {
+              setExistingProfilePicture(downloadURL);
+              localStorage.setItem('profilePicture', downloadURL);
+              setCroppedImage(null);
+              setError(null);
+            })
+            .catch((error) => {
+              console.error('Error getting download URL:', error);
+              setError('Failed to get download URL. Please try again later.');
+            });
+        }
+      );
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      setError('Failed to upload profile picture. Please try again later.');
+    }
   };
-
+  
   const handleDeleteProfilePicture = async () => {
     if (!existingProfilePicture) {
       console.log('No existing profile picture to delete.');
       return; // No existing profile picture to delete
     }
-
+  
     const userId = auth.currentUser.uid;
     const storage = getStorage();
     const profilePictureRef = ref(storage, `users/${userId}/profilePicture`);
-
+  
     try {
       await deleteObject(profilePictureRef);
       setExistingProfilePicture(null); // Reset existingProfilePicture state
@@ -123,6 +145,8 @@ const UploadPFP = () => {
       setError('Failed to delete profile picture. Please try again later.');
     }
   };
+
+
 
   return (
     <div className='flex flex-col'>
@@ -147,10 +171,35 @@ const UploadPFP = () => {
           className="w-36 h-36 max-w-36 max-h-36 rounded-full object-cover border border-gray-400 m-4"
         />
       )}
+  
+      {/* Image Cropper */}
+      {image && (
+        <ReactCrop
+          src={URL.createObjectURL(image)}
+          crop={crop}
+          onChange={onCropChange}
+          onComplete={onCropComplete} // Add this line
+          onImageLoaded={onImageLoaded} // Call getCroppedImage after the image is loaded
+        />
+      )}
+
       
+      {croppedImage && (
+        <div>
+          <h2>Cropped Image Preview:</h2>
+          <img
+            src={URL.createObjectURL(croppedImage)}
+            alt="Cropped"
+            className="w-36 h-36 max-w-36 max-h-36 rounded-full object-cover border border-gray-400 m-4"
+          />
+        </div>
+      )}
+  
       {error && <div>{error}</div>}
     </div>
   );
+  
+  
 };
 
 export default UploadPFP;
