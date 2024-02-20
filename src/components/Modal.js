3dedfd4e1d298df5e93a24ce099f5c3f8b4968cb
Nathan Race <racecn@gmail.com>
2024-02-19 20:10:43 -0500

    More comments

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,9 +4,11 @@ import { Registration } from './Registration';
 import '../styles/modal.css';
 
 function Modal() {
+    // State variables for modal visibility and mode (sign-in or sign-up)
     const [showModal, setShowModal] = useState(false);
     const [mode, setMode] = useState('sign-in');
 
+    // Function to toggle modal visibility
     const toggleModal = () => {
         setShowModal(!showModal);
         document.body.classList.toggle('active-modal');
@@ -18,13 +20,23 @@ function Modal() {
 
     return (
         <div>
+            {/*Button to open modal*/}
             <button onClick={toggleModal}>Open Modal</button>
+
             {showModal && (
                 <div className="fixed z-50 inset-0 overflow-y-auto">
                     <div className="flex items-center justify-center min-h-screen">
+
+                        {/*Modal overlay, the dark background*/}
                         <div className="modal-overlay absolute inset-0 bg-gray-500 opacity-75" onClick={toggleModal}></div>
+                        
+                        {/*Modal content container*/}
                         <div className="modal-content bg-white p-4 rounded-lg max-w-md" style={{ height: '700px' }}>
+                            
+                            {/*Close button*/}
                             <button className="close-modal text-red-400 absolute top-0 right-0 px-4 py-2" onClick={toggleModal}>Close</button>
+                            
+                            {/*Modal content*/}
                             <div className="modal-body">
                                 {mode === 'sign-in' ? (
                                     <SignIn />
@@ -32,6 +44,8 @@ function Modal() {
                                     <Registration />
                                 )}
                             </div>
+
+                            {/*Login to Register switch*/}
                             <div className="mode-switch mb-4">
                                 {mode === 'sign-in' && (
                                     <>
