10b5190c290010a5a6d45199e6df11e50e203a99
Nathan Race <racecn@gmail.com>
2024-02-29 13:26:43 -0500

    Improve lazyImage load time

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -1,37 +1,46 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 
-const LazyImage = ({ src, alt }) => {
+const LazyImage = ({ src, alt, placeholder }) => {
   const imgRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  const observer = useRef(null);
+
+  const handleIntersect = useCallback((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setIsVisible(true);
+        observer.current.unobserve(entry.target);
+      }
+    });
+  }, []);
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-          observer.unobserve(entry.target);
-        }
-      });
-    });
+    observer.current = new IntersectionObserver(handleIntersect, { threshold: 0.1 });
 
     if (imgRef.current) {
-      observer.observe(imgRef.current);
+      observer.current.observe(imgRef.current);
     }
 
     return () => {
       if (imgRef.current) {
-        observer.unobserve(imgRef.current);
+        observer.current.unobserve(imgRef.current);
       }
     };
-  }, []);
+  }, [handleIntersect]);
 
   return (
     <img
       ref={imgRef}
-      src={isVisible ? src : ''}
+      src={isVisible ? src : placeholder.iconName}
       alt={alt}
+      loading="lazy"
     />
   );
+};
+
+LazyImage.defaultProps = {
+  placeholder: faSpinner
 };
 
 export default LazyImage;
