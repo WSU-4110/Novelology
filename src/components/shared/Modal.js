0df4bdeb3516813973c4f00d6160d96ae2163d42
Jad Aoun <103859749+JadAoun1@users.noreply.github.com>
2024-03-05 08:14:26 -0500

    Update Modal.js

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -2,39 +2,42 @@ import React, { useState } from 'react';
 import { SignIn } from '../user/SignIn';
 import { Registration } from '../user/Registration';
 import '../../styles/modal.css';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function Modal() {
-    // State variables for modal visibility and mode (sign-in or sign-up)
+
+    const navigate = useNavigate();
     const [showModal, setShowModal] = useState(false);
     const [mode, setMode] = useState('sign-in');
 
-    // Function to toggle modal visibility
     const toggleModal = () => {
-        setShowModal(!showModal);
-        document.body.classList.toggle('active-modal');
+        navigate('/sign-in'); 
     };
 
     const handleModeChange = (newMode) => {
         setMode(newMode);
+
     };
 
     return (
         <div>
-            <button onClick={toggleModal}>Sign In</button> 
+            <button onClick={toggleModal}>Sign In</button>
             {showModal && (
                 <div className="fixed z-50 inset-0 overflow-y-auto">
                     <div className="flex items-center justify-center min-h-screen">
 
-                        {/*Modal overlay, the dark background*/}
+                        {/* Modal overlay, the dark background */}
                         <div className="modal-overlay absolute inset-0 bg-gray-500 opacity-75" onClick={toggleModal}></div>
                         
-                        {/*Modal content container*/}
-                        <div className="modal-content bg-white p-4 rounded-lg max-w-md" style={{ height: '700px' }}>
+                        {/* Modal content container */}
+                        <div className="modal-content bg-white p-4 rounded-lg" style={{ width: '100vw', height: '100vh' }}>
                             
-                            {/*Close button*/}
+                            {/* Close button */}
                             <button className="close-modal text-red-400 absolute top-0 right-0 px-4 py-2" onClick={toggleModal}>Close</button>
                             
-                            {/*Modal content*/}
+                            {/* Modal content */}
                             <div className="modal-body">
                                 {mode === 'sign-in' ? (
                                     <SignIn />
@@ -43,7 +46,7 @@ function Modal() {
                                 )}
                             </div>
 
-                            {/*Login to Register switch*/}
+                            {/* Login to Register switch */}
                             <div className="mode-switch mb-4">
                                 {mode === 'sign-in' && (
                                     <>
