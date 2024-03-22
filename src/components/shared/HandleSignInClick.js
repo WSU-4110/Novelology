018511584c2cde39bb43cdea75ecdeb5e4bb5fd8
Jad Aoun <103859749+JadAoun1@users.noreply.github.com>
2024-03-21 21:40:20 -0400

    Create HandleSignInClick.js

---
+++ src/components/shared/HandleSignInClick.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Modal() {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate('/sign-in'); 
+    };
+
+    return (
+        <div>
+            <button onClick={handleSignInClick}>Sign In</button>
+        </div>
+    );
+}
+
+export default Modal;
